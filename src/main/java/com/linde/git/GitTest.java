088a3a5fb4c6c2482c622a14549eccc41325e0e0
Linde <1030907322@qq.com>
2023-03-04 17:26:06 +0800

    train git_test commit

--- src/main/java/com/linde/git/GitTest.java
+++ src/main/java/com/linde/git/GitTest.java
@@ -4,16 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("-------------------------------");
-        System.out.println("hello git2!");
-        System.out.println("-------------------------------");
-        System.out.println("hello git3!");
-        System.out.println("-------------------------------");
-        System.out.println("hello git4 hot-fix!");
-        System.out.println("-------------------------------");
-        System.out.println("hello git4 hot-fix! test");
-        System.out.println("-------------------------------");
-        System.out.println("hello git4 master test!");
-        System.out.println("-------------------------------");
-        System.out.println("hello git4 master push test!");
+        System.out.println("git_test remote");
     }
 }
