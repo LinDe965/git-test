882157e6cf77a8cc80818f90c97b9c472eb34bc4
LinDe <92204919+LinDe965@users.noreply.github.com>
2023-03-04 17:12:32 +0800

    pull commit

--- src/main/java/com/linde/git/GitTest.java
+++ src/main/java/com/linde/git/GitTest.java
@@ -3,10 +3,15 @@ package com.linde.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("-------------------------------");
         System.out.println("hello git2!");
+        System.out.println("-------------------------------");
         System.out.println("hello git3!");
+        System.out.println("-------------------------------");
         System.out.println("hello git4 hot-fix!");
+        System.out.println("-------------------------------");
         System.out.println("hello git4 hot-fix! test");
+        System.out.println("-------------------------------");
         System.out.println("hello git4 master test!");
         System.out.println("-------------------------------");
         System.out.println("hello git4 master push test!");
