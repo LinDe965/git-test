33fa058666e49685773d2d71a0d293b59660462c
Linde <1030907322@qq.com>
2023-03-04 16:30:30 +0800

    second commit

--- src/main/java/com/linde/git/GitTest.java
+++ src/main/java/com/linde/git/GitTest.java
@@ -3,5 +3,6 @@ package com.linde.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
